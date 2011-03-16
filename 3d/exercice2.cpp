d5871f447bb6f37ae7acd749f367b7f70dffc83a
Julien Limoges <dev.limoges@gmail.com>
2011-03-16 09:08:41 +0100

    working on 3d/excercice2

--- 3d/exercice2.cpp
+++ 3d/exercice2.cpp
@@ -1,22 +1,47 @@
+#if defined(__APPLE__)
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GLUT/glut.h>
+#endif
 
+#include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <float.h>
+
+typedef struct rect 
+{
+    int Width;
+    int Height;
+    rect(int w, int h) : Width(w), Height(h) {}
+} rect;
+
+typedef struct vec3f
+{
+    float X;
+    float Y;
+    float Z;
+    vec3f(float x, float y, float z) : X(x), Y(y), Z(z) {}
+} vec3f;
 
 void Display();
 void Resize(const int x, const int y);
 void Special(int key, int x, int y);
 void Keyboard(unsigned char key, int x, int y);
+void Mouse(int button, int state, int x, int y);
 void randomizeClearColor();
+void findClosest(int x, int y);
+vec3f getOpenGLCoordFromScreenCoord(const int x, const int y);
 
-typedef struct rect 
-{
-    int width;
-    int height;
-    rect(int w, int h) : width(w), height(h) {}
-} rect;
+float vertices[6] = { -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f };
+float size = 0.01f;
+bool selected = false;
+float * vertex = NULL;
 
 int main(int argc, char ** argv)
 {
@@ -25,10 +50,9 @@ int main(int argc, char ** argv)
     rect window(800,600);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(window.width, window.height);
+    glutInitWindowSize(window.Width, window.Height);
 
 	glutInitWindowPosition(10,10);
-	glutInitWindowSize(500,500);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutCreateWindow("Exercice 3 Image");
 
@@ -36,32 +60,48 @@ int main(int argc, char ** argv)
 	glutReshapeFunc(Resize);
 	glutSpecialFunc(Special);
 	glutKeyboardFunc(Keyboard);
-    
+    glutMouseFunc(Mouse);
+  
 	glClearColor(1.0,0.5,1.0,1.0);
 	glutMainLoop();
 }
 
 void Display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
 	glMatrixMode(GL_MODELVIEW);
 
     glColor3f(0.0f, 0.0f, 0.0f);
     glBegin(GL_TRIANGLES);
-        glVertex3f(-1.0f, 0.0f, 0.0f);
-        glVertex3f(1.0f, 0.0f, 0.0f);
-        glVertex3f(0.0f, 1.0f, 0.0f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(vertices[0], vertices[1], 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(vertices[2], vertices[3], 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(vertices[4], vertices[5], 0.0f);
     glEnd();
+    
+    if (selected)
+    {
+        glColor3f(0.0f, 0.0f, 0.0f);
+        glBegin(GL_QUADS);
+            glVertex3f(vertex[0] - size, vertex[1] - size, 0.0f);
+            glVertex3f(vertex[0] - size, vertex[1] + size, 0.0f);
+            glVertex3f(vertex[0] + size, vertex[1] + size, 0.0f);
+            glVertex3f(vertex[0] + size, vertex[1] - size, 0.0f);
+        glEnd();
+    }
 
 	glutSwapBuffers();
 }
 
 void Resize(const int x, const int y)
 {
-	glViewport(0,0,x,y);
+	glViewport(0, 0, x, y);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0,x,0,y);
+	gluOrtho2D(0, x, 0, y);
 }
 
 void Special(int key, int x, int y)
@@ -100,6 +140,21 @@ void Keyboard(unsigned char key, int x, int y)
 	glutPostRedisplay();
 }
 
+void Mouse(int button, int state, int x, int y)
+{
+    printf("(%d, %d)\n", x, y);
+    if (GLUT_UP)
+    {
+        switch (button)
+        {
+            case GLUT_LEFT_BUTTON:
+                findClosest(x, y);
+                break;
+        }
+        glutPostRedisplay();
+    }
+}
+
 float getRandomClampf()
 {
     return rand() / (((float) RAND_MAX) + 1);
@@ -111,4 +166,51 @@ void randomizeClearColor()
     float green = getRandomClampf();
     float blue = getRandomClampf();
     glClearColor(red, green, blue, 1.0);
+}
+
+float distance(int ax, int ay, int bx, int by)
+{
+    float distance = sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
+    return distance;
+}
+
+void findClosest(int x, int y)
+{
+    float min = FLT_MAX, dist = FLT_MAX;
+    int imin = 0;
+
+    for (int i = 0, b = 0; b < 3; b++, i += 2)
+    {
+        dist = distance(vertices[i], vertices[i], x, y);
+        if (dist < min);
+        {
+            //printf("dist:%f", dist);
+            min = dist;
+            imin = i;
+        }
+    }
+    //printf("distance(%d);", dist);
+    selected = true;
+    vertex = &vertices[imin];
+    //printf("/[%f][%f]/\n", vertex[0], vertex[1]);
+    fflush(stdout);
+}
+
+vec3f getOpenGLCoordFromScreenCoord(const int x, const int y)
+{
+    GLint viewport[4];
+    GLdouble modelview[16];
+    GLdouble projection[16];
+    GLfloat screenx, screeny, screenz;
+    GLfloat posx, posy, posz;
+
+    glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+    glGetDoublev(GL_PROJECTION_MATRIX, projection);
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    screenx = (GLfloat) x;
+    screeny = (GLfloat) viewport[3] - (GLfloat) y;
+    glReadPixels(x, (GLint) screeny, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &screenz);
+    gluUnProject(screenx, screeny, screenz, modelview, projection, viewport, posx, posy, posz);
+    return vec3f(posx, posy, posz);
 }
